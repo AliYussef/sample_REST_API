5695337eaa7e80236b3d12852b25fb1d0b6d3620
AliYussef <36632054+AliYussef@users.noreply.github.com>
2019-05-06 15:15:43 +0200

    Update AnimeService.java

--- src/main/java/nl/inholland/service/AnimeService.java
+++ src/main/java/nl/inholland/service/AnimeService.java
@@ -5,34 +5,61 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class AnimeService {
 
     private List<Anime> animes = new ArrayList<>(
             Arrays.asList(
-                    new Anime(1, "Fullmetal Alchemist: Brotherhood", 9.24f),
-                    new Anime(2, "Hunter x Hunter", 9.12f),
-                    new Anime(3, "Dragon Ball Z", 8.28f),
-                    new Anime(4, "JoJo's Bizarre Adventure: Stardust Crusaders", 8.25f),
-                    new Anime(5, "One Piece", 8.53f),
-                    new Anime(6, "Naruto: Shippuuden", 8.20f)
+                    new Anime(1, "Fullmetal Alchemist: Brotherhood", "Fantasy", 9.24f),
+                    new Anime(2, "Hunter x Hunter", "Adventure", 9.12f),
+                    new Anime(3, "Dragon Ball Z", "Supernatural", 8.28f),
+                    new Anime(4, "JoJo's Bizarre Adventure: Stardust Crusaders", "Adventure", 8.25f),
+                    new Anime(5, "One Piece", "Adventure", 8.53f),
+                    new Anime(6, "Naruto: Shippuuden", "Adventure", 8.20f)
             )
     );
 
-    public List<Anime> getAnimes() {
-        return animes;
+    public List<Anime> getAnimes(boolean sorted, double minumum, int pageSize) {
+        Stream<Anime> animeStream = animes.stream()
+                .filter(anime -> anime.getRanking() >= minumum);
+
+        if (sorted) {
+            animeStream = animeStream
+                    .sorted(Comparator.comparingDouble(Anime::getRanking));
+        }
+
+        return animeStream
+                .limit(pageSize)
+                .collect(Collectors.toList());
+
     }
 
-    public Anime getAnime(int id) {
+    public Anime getAnimeById(int id) {
 
-        for (Anime anime : animes) {
-            if (anime.getId() == id) {
-                return anime;
-            }
-        }
-        return null;
+        return animes.stream()
+                .filter(anime -> anime.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Anime getAnimeByTitle(String title) {
+
+        return animes.stream()
+                .filter(anime -> anime.getTitle().equals(title))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Anime> getAnimesByGenre(String genre) {
+
+        return animes.stream()
+                .filter(anime -> anime.getGenre().toLowerCase().equals(genre.toLowerCase()))
+                .collect(Collectors.toList());
     }
 
     public void addAnime(Anime anime) {
@@ -45,6 +72,7 @@ public class AnimeService {
             if (anime.getId() == id) {
                 anime1.setTitle(anime.getTitle());
                 anime1.setRanking(anime.getRanking());
+                anime1.setGenre(anime.getGenre());
             }
         }
     }
